4778ad117948442617f872ea85e2aac6099d1161
Maria Bazhenova <yankeevsdixie@gmail.com>
2025-10-09 21:51:22 +0300

    added logic for opening the menu on mobile devices

---
+++ src/components/Header/MobileNav.jsx
@@ -0,0 +1,24 @@
+
+
+const MobileNav = () => {
+  return (
+      <nav className='mobile-nav'>
+      <ul>
+       <li>
+        <a href='#'>Главная</a>
+       </li>
+       <li>
+        <a href='#'>Цены</a>
+       </li>
+       <li>
+        <a href='#'>Блог</a>
+       </li>
+       <li>
+        <a href='#'>О компании</a>
+       </li>
+      </ul>
+     </nav>
+  )
+}
+
+export default MobileNav

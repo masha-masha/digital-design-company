8c1d640f22fbd76aa9e376744692ac36e34e4f0f
Maria Bazhenova <yankeevsdixie@gmail.com>
2025-10-15 15:27:43 +0300

    component Portfolio and PortfolioCard are styled for mobile devices

--- src/components/Portfolio/PortfolioCard.jsx
+++ src/components/Portfolio/PortfolioCard.jsx
@@ -1,11 +1,11 @@
-const PortfolioCard = ({ linkToImage, title, description}) => {
-  return (
-    <li className="portfolio-item">
+const PortfolioCard = ({ linkToImage, title, description }) => {
+ return (
+  <li className="portfolio-item">
    <img src={linkToImage} alt={title} />
    <h3>{title}</h3>
    <p>{description}</p>
   </li>
-  )
-}
+ );
+};
 
-export default PortfolioCard
+export default PortfolioCard;
